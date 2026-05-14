d95fa1bde50429e60bb4ffffb942a494df661a21
chrisnie-airtasker <zhengtong.nie@airtasker.com>
2026-05-14 14:10:01 +1000

    chore:use oidc

--- dev/main.tf
+++ dev/main.tf
@@ -4,3 +4,22 @@ module "dev" {
   # Change 0 to 1 and open a pull request to trigger Terrateam
   null_resource_count = 2
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+data "aws_caller_identity" "current" {}
+
+output "caller_arn" {
+  value = data.aws_caller_identity.current.arn
+}
