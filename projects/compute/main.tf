d5bd68bda7a045275ac4a8d24faa08441a6f2374
chrisnie-airtasker <zhengtong.nie@airtasker.com>
2026-05-15 09:27:56 +1000

    add drift detection config

--- projects/compute/main.tf
+++ projects/compute/main.tf
@@ -25,6 +25,12 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
+provider "aws" {
+  region = "ap-southeast-2"
+  profile = "engineer"
+  alias = "engineer"
+}
+
 locals {
   resource_count = terraform.workspace == "prod" ? 4 : 2
 }
@@ -33,6 +39,32 @@ module "compute" {
   source         = "../../modules"
   project_name   = "compute"
   resource_count = local.resource_count
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "example" {
+  count         = terraform.workspace == "prod" ? 1 : 0
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
 
 data "aws_caller_identity" "current" {}
